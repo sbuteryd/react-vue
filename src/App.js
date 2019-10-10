a8b5d99543af897d906e57d55406b3b1603730ba
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-10 20:34:58 +0800

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Socks from './components/Socks'
+import blue from './assert/vmSocks-blue-onWhite.jpg'
+import green from './assert/vmSocks-green-onWhite.jpg'
 
 
 class App extends React.Component{
+  state = {
+    date :{
+      product:'Socks',
+      blue:blue,
+      green:green
+    }
+  }
   render() {
     return (
         <div>
-          <Socks/>
+          <Socks product={this.state.date} />
         </div>
     );
   }
--- src/components/Socks.js
+++ src/components/Socks.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './product_style.css'
 
 export default class Socks extends React.Component{
     render() {
+        const {product,blue,green} = this.props.product
         return (
-            <div>
-                Socks
+            <div className='product'>
+                <div className='product-image'>
+                    <img src={green} alt=""/>
+                </div>
+                <div className='product-info'>
+                    <h1>{product}</h1>
+                </div>
             </div>
         );
     }
